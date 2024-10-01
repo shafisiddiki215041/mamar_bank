175338e5f83f820b8487fd0d23743f389e428c5c
shafisiddiki215041 <140194303+shafisiddiki215041@users.noreply.github.com>
2024-10-01 16:35:38 +0600

    Update settings.py

--- mamar_Bank/settings.py
+++ mamar_Bank/settings.py
@@ -34,7 +34,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = [" https://mamar-bank-lzlt.onrender.com"," http://127.0.0.1:8000/"]
+CSRF_TRUSTED_ORIGINS = ["https://mamar-bank-lzlt.onrender.com", "https://127.0.0.1"]
+
 
 
 # Application definition
